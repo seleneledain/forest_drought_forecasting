c80cc9dd79295daec09b00330063bca78281d52d
Sélène Hui-Miao Ledain <selene.ledain@epfl.ch>
2023-08-10 13:45:12 +0200

    Setup of data creation in workstation

--- data_downloading/config_drought.py
+++ data_downloading/config_drought.py
@@ -1,10 +1,10 @@
 # Configuration file for minicube generation
 
-coord_list_paths = ['/Users/led/Desktop/sampling_rasters/coords_drought.txt'] 
+coord_list_paths = ['/data/scratch/selene/sampling_rasters/coords_drought.txt'] 
 
 specs = {
     "lon_lat": None, #(6.73570, 46.93912), # center pixel Creux du Van
-    "xy_shape": (60, 60), # width, height of cutout around center pixel
+    "xy_shape": (128, 128), # width, height of cutout around center pixel
     "resolution": 20, # in meters.. will use this on a local UTM grid..
     "time_interval": "2015-08-01/2021-12-31",
     "providers": [
@@ -21,14 +21,25 @@ specs = {
 
 
 specs_add_bands = {
-    "bands": ['SAND0_5', 'FED100_200', 'NDVI', "FOREST_MASK", "DEM", "DROUGHT_MASK"],
-    "static_dir": '/Users/led/Desktop/Master_Thesis/Data/original_data/soil_maps/' # Path_to_local_data
+    "bands": ['NDVI', "FOREST_MASK", "DEM", "DROUGHT_MASK", 
+              "CLAY0_5", "CLAY100_200", "CLAY15_30", "CLAY30_60", "CLAY5_15", "CLAY60_100", 
+              "FED0_5", "FED100_200", "FED15_30", "FED30_60", "FED5_15", "FED60_100", 
+              "FC", "FH",
+              "GRAV0_5", "GRAV100_200", "GRAV15_30", "GRAV30_60", "GRAV5_15", "GRAV60_100",
+              "SAND0_5", "SAND100_200", "SAND15_30", "SAND30_60", "SAND5_15", "SAND60_100",
+              "SDEP",
+              "CARB0_5", "CARB100_200", "CARB15_30", "CARB30_60", "CARB5_15", "CARB60_100",
+              "PH0_5", "PH100_200", "PH15_30", "PH30_60", "PH5_15", "PH60_100",
+              "DEM_100", "DEM_500", "slope", "easting", "northing", "rugg", "curv", "twi",
+              "slope_100", "easting_100", "northing_100", "rugg_100", "curv_100", "twi_100",
+              "slope_500", "easting_500", "northing_500", "rugg_500", "curv_500", "twi_500"],
+    "static_dir": '/data/scratch/selene/static_data/' # Path_to_local_data
 }
 
 bands_to_drop = ["s2_mask", "s2_avail", "s2_SCL", "to_sample", "FOREST_MASK", "DROUGHT_MASK"]
 
 # Where to save data
-root_dir = '/Users/selene/Desktop/Unibe/New code/'
+root_dir = '/data/scratch/selene/'
 split = 'train_drought'
 
 # Format of data sample
